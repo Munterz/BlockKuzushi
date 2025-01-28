34a0890bc2ad0b207eb9f9af1a0507850258d478
Munterz <munterskevin@hotmail.com>
2025-01-29 00:08:01 +0100

    Lagt till projektfiler, ignorerar onödiga filer

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "Block Kuzushi");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib test", 300, 280, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
