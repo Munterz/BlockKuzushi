96f3b35e1e181b6a7ff8403430a520affc0503af
Munterz <munterskevin@hotmail.com>
2025-01-30 21:16:33 +0100

    Paddel & Frame

--- main.c
+++ main.c
@@ -1,15 +1,34 @@
 #include "raylib.h"
 
 int main() {
-    InitWindow(800, 600, "Block Kuzushi");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Block Kuzushi");
+    SetTargetFPS(60);
+
+
+    Rectangle paddle = { screenWidth / 2 - 50, screenHeight - 40, 100, 10 };
+    int paddleSpeed = 8;
 
     while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_LEFT) && paddle.x > 0) {
+            paddle.x -= paddleSpeed;
+        }
+        if (IsKeyDown(KEY_RIGHT) && paddle.x < screenWidth - paddle.width) {
+            paddle.x += paddleSpeed;
+        }
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Raylib test", 300, 280, 20, DARKGRAY);
+        ClearBackground(BLACK);
+
+
+        DrawRectangleRec(paddle, WHITE);
+
         EndDrawing();
     }
-//test
+
     CloseWindow();
     return 0;
 }
